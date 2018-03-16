6ccde3ae46f38d6136ef9aa7f4dcc2c127df95e6
Jason Lestina <coda19@gmail.com>
2018-03-16 01:16:31 -0400

    testimonials dynamic and updated

--- js/main.js
+++ js/main.js
@@ -1,29 +1,49 @@
-window.setInterval(rotateTestimonials, 5000);
+fetch('http://www.johnlestinajr.com/data/testimonials.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json){
+    var testimonials = json.testimonials;
 
-var testimonialTracker = 1;
-function rotateTestimonials() {
-  var testimonials = document.getElementsByClassName('quote');
+    cycleTestimonials(testimonials);
 
-  if (testimonialTracker >= testimonials.length) {
+    window.setInterval(function() {
+      cycleTestimonials(testimonials);
+    }, 6000)
+
+  })
+
+
+var testimonialTracker = 0;
+function cycleTestimonials(testimonials) {
+
+  var keys = Object.keys(testimonials);
+
+  if (testimonialTracker >= keys.length) {
     testimonialTracker = 0;
   }
 
-  for (var i = 0; i < testimonials.length; i++) {
-
-    for (var j = 0; j < testimonials[i].children.length; j++) {
-
-      if (i === testimonialTracker) {
-        testimonials[i].classList.remove('hidden');
-        testimonials[i].children[j].classList.remove('hidden');
-      } else {
-        testimonials[i].classList.add('hidden')
-        testimonials[i].children[j].classList.add('hidden')
-      }
-
-    }
-
-  }
-
+  buildHtml(testimonials[keys[testimonialTracker]]);
   testimonialTracker++;
+
+}
+
+function buildHtml(testimonial) {
+
+  var testimonialBox = document.getElementById('testimonials')
+  testimonialBox.innerHTML = '';
+
+  var quoteDiv = document.createElement('div');
+  quoteDiv.classList.add('quote');
+
+  var quoteP = document.createElement('p');
+  quoteP.innerHTML = testimonial.quote;
+
+  var name = document.createElement('h1');
+  name.innerHTML = '- ' + testimonial.name;
+
+  quoteDiv.appendChild(quoteP);
+  quoteDiv.appendChild(name);
+  testimonialBox.appendChild(quoteDiv);
 
 }
